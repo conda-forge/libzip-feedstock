b2f3744b4067a85b2f390767b30cad4c17152ae2
Kai Tietz <ktietz@anaconda.com>
2021-12-21 18:31:35 +0100

    update recipe dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,18 @@
 
 mkdir build && cd build
 
-cmake \
+cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_INCLUDEDIR=include \
+    -DENABLE_COMMONCRYPTO=OFF \
+    -DENABLE_GNUTLS=OFF \
+    -DENABLE_MBEDTLS=OFF \
+    -DENABLE_OPENSSL=ON \
+    -DENABLE_WINDOWS_CRYPTO=OFF \
+    -DENABLE_BZIP2=ON \
+    -DENABLE_LZMA=OFF \
+    -DENABLE_ZSTD=OFF \
     ..
 make
 make test
